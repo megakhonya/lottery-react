700f83efd81e6256bb1c999789f844bfc65ec14d
Meg Akhonya <musinzimeg@gmail.com>
2024-06-01 20:09:50 +0300

    upload code

---
+++ src/lottery.js
@@ -0,0 +1,10 @@
+import web3 from './web3';
+
+const address = '0x5C9978DEcb168CcC30F1F0F6d95267C0E2cBb1c4';
+
+const abi = 
+[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"enter","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"getPlayers","outputs":[{"internalType":"address payable[]","name":"","type":"address[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"manager","outputs":[{"internalType":"address payable","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"pickWinner","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"players","outputs":[{"internalType":"address payable","name":"","type":"address"}],"stateMutability":"view","type":"function"}];
+
+export default new web3.eth.Contract(abi, address);
+
+
